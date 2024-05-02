1c1de233eb93141a2b4a280f1e7ac25867a58f64
Lu-jiajun <Lu_jiajun@outlook.com>
2024-05-03 00:28:55 +0800

    5-3

---
+++ gatway/src/main/java/fun/catadopt/gatway/GatwayApplication.java
@@ -0,0 +1,16 @@
+package fun.catadopt.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GatwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatwayApplication.class, args);
+    }
+
+}
---
+++ gatway/src/main/java/fun/catadopt/gatway/config/CustomCorsConfiguration.java
@@ -0,0 +1,27 @@
+package fun.catadopt.gatway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CustomCorsConfiguration {
+
+//    @Bean
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 配置跨域的信息
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        // SpringBoot升级到2.4.0 之后需要使用该配置
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**",configuration);
+        return new CorsWebFilter(source);
+    }
+}
